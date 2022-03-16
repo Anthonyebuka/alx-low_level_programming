c39f4dbb7ea76c06f3b5d136e7e75d144a0f1167
Anthonyebuka <anthonyebuka01@gmail.com>
2022-03-16 05:36:06 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * this program prints _putchar followed by a new line
- * Return: Always 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+
 	return (0);
 }
